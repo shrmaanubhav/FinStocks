7ed43f78355407c20b1efbd81d1d4921669cd881
Soham-Donode <sohamdonode001@gmail.com>
2026-01-23 23:44:55 +0530

    forntend inti

---
+++ frontend/app/scroll/page.tsx
@@ -0,0 +1,10 @@
+import { MacbookScroll } from "@/components/ui/macbook-scroll";
+import { div } from "framer-motion/client";
+import { Badge } from "lucide-react";
+
+export default function AboutPage() {
+  return (
+    <div>hi</div>
+  );
+}
+
