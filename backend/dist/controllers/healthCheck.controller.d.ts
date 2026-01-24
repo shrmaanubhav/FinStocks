21c6a11268d5d8aa47f47bd9d0eafbd242659914
Soham-Donode <sohamdonode001@gmail.com>
2026-01-24 19:54:59 +0530

    remove backend

--- backend/dist/controllers/healthCheck.controller.d.ts
+++
@@ -1,3 +0,0 @@
-import type { Request, Response } from "express";
-export declare const healthCheck: (req: Request, res: Response) => void;
-//# sourceMappingURL=healthCheck.controller.d.ts.map
--- backend/dist/utils/apiError.d.ts
+++
@@ -1,9 +0,0 @@
-declare class apiError extends Error {
-    statuscode: number;
-    data: any;
-    success: boolean;
-    errors: any[];
-    constructor(statuscode: number, message?: string, errors?: any[], stack?: string);
-}
-export { apiError };
-//# sourceMappingURL=apiError.d.ts.map
