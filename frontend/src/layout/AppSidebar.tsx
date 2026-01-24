76de99c1ef3a9079a65f585d85b68a48968eb531
Soham-Donode <sohamdonode001@gmail.com>
2026-01-24 13:06:42 +0530

    removed add

--- frontend/src/layout/AppSidebar.tsx
+++ frontend/src/layout/AppSidebar.tsx
@@ -374,7 +374,6 @@ const AppSidebar: React.FC = () => {
             </div>
           </div>
         </nav>
-        {isExpanded || isHovered || isMobileOpen ? <SidebarWidget /> : null}
       </div>
     </aside>
   );
